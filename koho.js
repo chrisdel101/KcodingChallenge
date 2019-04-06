26865b0ddaa63323a259851c3ffadd57f48e437f
Chris Del <drumgod101@gmail.com>
2019-04-06 16:54:59 -0400

    test and funcs working for daily check

--- koho.js
+++ koho.js
@@ -1,120 +1,183 @@
 let DB = {}
-function handleAccount (id, load_amount, date, customerId){
-        // if not there, create and add
-        if(!DB[customerId]){
-            DB[customerId] = createAccount(id, remove$(load_amount), date, customerId)
-            return true
-        } else { // just add funds
-            // if 3 transactions break
-            if(!verifyAmountDay(DB[customerId], date)) return false
-            // console.log(verifyAmountDay(DB[customerId], date))
-            countTranxDay(DB[customerId])
-            DB[customerId][date] = addFunds(id, remove$(load_amount), date, customerId)
-            return true
-        }
-        return false
+
+function handleAccount(id, load_amount, date, customerId) {
+	// if not there, create and add
+	if(!DB[customerId]) {
+		// check for 5000
+		if(!verifyUnder5000(load_amount)) return false
+		// console.log('TOP')
+		DB[customerId] = createAccount(id, remove$(load_amount), date, customerId)
+		return true
+	} else { // just add funds
+		console.log('BOTTOM')
+		// if 3 transactions break
+		// console.log('above', DB[customerId])
+		if(!verifyTranxDay(DB[customerId], date, customerId)) return false
+		// console.log('CHECK', verifyAmountPerDay(DB[customerId], date))
+		if(!verifyAmountPerDay(DB[customerId], date, load_amount)) return false
+
+		// console.log(verifyAmountPerDay(DB[customerId], date))
+		getTranxDay(DB[customerId])
+		console.log('below', DB[customerId])
+		DB[customerId][date] = addFunds(id, remove$(load_amount), customerId, date)
+		console.log('after load', DB[customerId])
+		return true
+	}
+	return false
 }
+
 // takes date and returns a easy to read string
-function makeDateObj(dateStr){
-    const date = new Date(dateStr)
-    const y = date.getFullYear()
-    const m = date.getMonth() + 1
-    const d = date.getDate()
-    let tranxKey = `${y}-${m}-${d}`
-    return tranxKey
+function makeDateObj(dateStr) {
+	// console.log("DS", dateStr)
+	const date = new Date(dateStr)
+	const y = date.getFullYear()
+	const m = date.getMonth() + 1
+	const d = date.getDate()
+	let tranxKey = `${y}-${m}-${d}`
+	// console.log('tra', tranxKey)
+	return tranxKey
 }
 // takes a customer obj returns all tranx per day
-function countTranxDay(custObj){
-    let tranxDay = {}
-    // once object is made go through and count transactions
-    Object.keys(custObj).forEach(dateKey => {
-        let tranxKey = makeDateObj(dateKey)
-        // console.log('traxKEY', tranxKey)
-        // console.log('dateKey', dateKey)
-            if(!tranxDay[tranxKey]){
-                tranxDay[tranxKey] = 1
-                // console.log('above')
-                // console.log('tran', tranxDay)
-            } else {
-                tranxDay[tranxKey]++
-            }
-    })
-    // console.log('done', tranxDay)
-    return tranxDay
+function getTranxDay(custObj, id) {
+	let tranxDay = {}
+	// once object is made go through and count transactions
+	Object.keys(custObj).forEach(dateKey => {
+		let tranxKey = makeDateObj(dateKey)
+		// console.log('traxKEY', tranxKey)
+		// console.log('dateKey', dateKey)
+		// if date not there, add
+		if(!tranxDay[tranxKey]) {
+			tranxDay[tranxKey] = 1
+			// console.log('above')
+			// console.log('tran', tranxDay)
+		} else {
+			// if dates match increment
+			tranxDay[tranxKey]++
+		}
+	})
+	// console.log('done', tranxDay)
+	return tranxDay
+}
+// count all tranactions matching curent date and add up
+function getDailySum(custObj, date) {
+
+	let currentDate = makeDateObj(date)
+	// console.log(currentDate)
+	let tranxPerDay = getTranxDay(custObj)
+	// console.log('trans', tranxPerDay)
+	for(var tranxDate of Object.keys(tranxPerDay)) {
+		// console.log('date', tranxDate)
+		if(currentDate === tranxDate) {
+			// console.log("currentDate", currentDate)
+			// console.log("tranxDate", tranxDate)
+			// console.log('CUS', custObj)
+			const amounts = Object.values(custObj).map(val => {
+				return val.loadAmount
+			})
+			// console.log('ammounts', amounts)
+			const sum = amounts.reduce((acc, cur) => {
+				return parseFloat(acc) + parseFloat(cur)
+			})
+			// console.log('SUM',sum)
+			return sum
+		}
+	}
+	// if no days match
+	return 0
+}
+// takes customer obj and date str
+function verifyAmountPerDay(custObj, date, load_amount) {
+	// console.log(load_amount)
+	// console.log(date)
+	let dailySum = getDailySum(custObj, date)
+	if(parseFloat(dailySum) + parseFloat(remove$(load_amount)) > 5000) {
+		console.log("Maximum depoist of 5000 reached. Cannot add.")
+		return false
+	} else {
+		// console.log("amount per day verified")
+		return true
+	}
 }
 // takes in customerObj from DB and date of current tranx
-function verifyAmountDay(custID, date){
-    // console.log(custID)
-    let newDate = makeDateObj(date)
-    let tranxPerDay = countTranxDay(custID)
-    console.log('trans', tranxPerDay)
-// https://stackoverflow.com/questions/52528760/javascript-function-returns-true-when-it-should-return-false
-    for(var tranxDate of Object.keys(tranxPerDay)){
-        // check current date against all customer transactions
-        // console.log('currentDate', newDate)
-        // console.log('tranxDate', tranxDate)
-        if(newDate === tranxDate){
-            // console.log('amount', tranxPerDay[tranxDate])
-            if(parseInt(tranxPerDay[tranxDate]) >= 3){
-                console.error("Maximun 3 transactions per day reached")
-                return false
-            }
-        }
-    }
-    return true
-}
-function createAccount(id, load_amount, date, customerId){
-    // create and add funds
-    return {
-        [date]: addFunds(id, load_amount, date, customerId)
-    }
-}
-function verifyUnder5000(amount){
-    if(amount > 5000){
-        return false
-    }
-    return true
-}
-function verifyAmountWeek(){
-
-}
-function verifyTranxDay(custId){
-
+// checks for 3 transactions per day
+function verifyTranxDay(custObj, date, id) {
+	let currentDate = makeDateObj(date)
+	let tranxPerDay = getTranxDay(custObj)
+	// https://stackoverflow.com/questions/52528760/javascript-function-returns-true-when-it-should-return-false
+	for(var tranxDate of Object.keys(tranxPerDay)) {
+		// check current date against all customer transactions
+		// console.log('currentDate', currentDate)
+		// console.log('tranxDate', tranxDate)
+		if(currentDate === tranxDate) {
+			// console.log('amount', tranxPerDay[tranxDate])
+			if(parseInt(tranxPerDay[tranxDate]) >= 3) {
+				console.error("Maximun 3 transactions per day reached")
+				return false
+			}
+		}
+	}
+	// console.log('tranx per day verfied')
+	return true
 }
 
-function addFunds (id, load_amount){
-    if(verifyUnder5000){
-        // add funds only
-        return  {
-            tranxId: id,
-            loadAmount: load_amount
-        }
-    } else if(!verifyUnder5000){
-        console.error("$5000 max reached for the day")
-        return
-    }
+function createAccount(id, load_amount, date, customerId) {
+	// create and add funds
+	return {
+		[date]: addFunds(id, load_amount, customerId, date)
+	}
 }
-function remove$(str){
-    if(str.startsWith("$")){
-        str = str.split("")
-        str.shift()
-        str = str.join("")
-        return str
-    } else {
-        return str
-    }
+// logs needed
+function verifyUnder5000(amount) {
+	if(amount > 5000) {
+		return false
+	}
+	return true
+}
+
+function verifyAmountWeek() {
+
 }
 
 
-// console.log(createAccount("15888","$3318 .47","2000-01-01T00:00:00Z",  "528"))
-// handleAccount("15888","$400 .47","2000-01-01T00:00:00Z",  "528" )
-// handleAccount("15889","$3318 .47","1999-12-31T09:00:00Z",  "528" )
-// handleAccount("15883","$447","2001-02-01T00:00:00Z",  "528" )
-// handleAccount("15866","$447","2001-02-01T04:00:00Z",  "528" )
-// handleAccount("1585","$447","2001-02-01T06:00:00Z",  "528" )
-// handleAccount("15845","$500","2001-03-01T00:00:00Z",  "528" )
-// handleAccount("15876","$400 .47","2000-01-01T01:00:00Z",  "528" )
-// handleAccount("15864","$400 .47","2000-01-01T02:00:00Z",  "528" )
+function addFunds(id, load_amount, customer_id, date) {
+	// console.log("add", id, load_amount, customer_id)
+	if(verifyUnder5000) {
+		console.log('FUNDS ADDED:', load_amount)
+		// add funds only
+		return {
+			id: customer_id,
+			tranxId: id,
+			loadAmount: load_amount,
+			readAbleDate: makeDateObj(date)
+		}
+	} else if(!verifyUnder5000) {
+		console.error("$5000 max reached for the day")
+		return
+	}
+}
+
+function remove$(str) {
+	if(str.startsWith("$")) {
+		str = str.split("")
+		str.shift()
+		str = str.join("")
+		return str
+	} else {
+		return str
+	}
+}
+
+
+handleAccount("15888", "$400.47", "2000-01-01T00:00:00Z", "528")
+handleAccount("15889", "$4500.47", "1999-12-31T09:00:00Z", "528")
+handleAccount("15866", "700.47", "2000-01-01T02:00:00Z", "528")
+
+handleAccount("15883", "$447.", "2001-02-01T00:00:00Z", "528")
+handleAccount("15866", "$447.", "2001-02-01T04:00:00Z", "528")
+handleAccount("1585", "$447.", "2001-02-01T06:00:00Z", "528")
+handleAccount("15845", "$500.", "2001-03-01T00:00:00Z", "529")
+handleAccount("15864", "$400.47", "2000-01-01T02:00:00Z", "529")
+handleAccount("15876", "$400.47", "2000-01-01T01:00:00Z", "529")
 console.log('DB', DB)
 // customerId: {
 //     date: [
@@ -137,15 +200,16 @@ console.log('DB', DB)
 //     return `${year}-${month}-${day}`
 // }
 module.exports = {
-    handleAccount: handleAccount,
-    addFunds: addFunds,
-    verifyUnder5000: verifyUnder5000,
-    verifyAmountDay: verifyAmountDay,
-    verifyAmountWeek: verifyAmountWeek,
-    verifyTranxDay:verifyTranxDay,
-    countTranxDay: countTranxDay,
-    createAccount: createAccount,
-    remove$:remove$,
-    makeDateObj: makeDateObj
+	handleAccount: handleAccount,
+	addFunds: addFunds,
+	verifyUnder5000: verifyUnder5000,
+	verifyAmountWeek: verifyAmountWeek,
+	verifyTranxDay: verifyTranxDay,
+	getTranxDay: getTranxDay,
+	createAccount: createAccount,
+	remove$: remove$,
+	makeDateObj: makeDateObj,
+	getDailySum: getDailySum,
+	verifyAmountPerDay: verifyAmountPerDay
 
 }
--- koho.test.js
+++ koho.test.js
@@ -2,81 +2,120 @@ const assert = require('assert')
 const mocha = require('mocha')
 const customer = require('./koho')
 
-describe("Koho tests", function(){
-    describe("addFunds", function(){
-        it('returns an object', function(){
-            // let cusmtomer =
-            let res = customer.addFunds("1", "500")
-            assert.deepEqual(res,{ tranxId: '1', loadAmount: '500' })
-        })
-    })
-    describe("verifyUnder5000", function(){
-        it("returns true if amount is under 5000", function(){
-            let res = customer.verifyUnder5000(4000)
-            assert(res)
-        })
-        it('returns false if amount is over 5000', function(){
-            let res = customer.verifyUnder5000(5001)
-            assert(!res)
-        })
-    })
-    describe("verifyAmountDay", function(){
-        it("returns true if amount is under 5000 for day ", function(){
-            let res = customer.verifyAmountDay(22)
-            assert(res)
-        })
-    })
-    describe("countTranxDay", function(){
-        it("returns num of tranx per day ", function(){
-            let res = customer.countTranxDay(22)
-            assert(res)
-        })
-    })
-    describe("createAccount", function(){
-        it("returns a customer acccount obj", function(){
-            let res = customer.createAccount("15888","$3318 .47","2000-01-01T00:00:00Z",  "528")
-            assert.deepEqual(res,{ '2000-01-01T00:00:00Z': { tranxId: '15888', loadAmount: '$3318 .47' } })
-        })
-    })
-    describe("remove$", function(){
-        it("returns string with dollar sign", function(){
-            let res = customer.remove$("$1000")
-            assert(res === "1000")
-        })
-    })
-    describe("makeDateObj", function(){
-        it("returns date parts obj from date string", function(){
-            let res = customer.makeDateObj("1999-12-31T03:00:00Z")
-            console.log('res', res)
-            assert.deepEqual(res, "1999-12-30")
-        })
-    })
-    describe("countTranxDay", function (){
-        it("returns object of transactions per day", function(){
-            let date1 = "2000-01-01T00:00:00Z"
-            let sameDate1 = "1999-12-31T09:00:00Z"
-            let date2 = "2001-02-01T06:00:00Z"
-            let date3 = "2001-02-01T04:00:00Z"
-            let sameDate3 = "2001-02-01T00:00:00Z"
-            let custObj = {
-              [date1]: { tranxId: '15888', loadAmount: '400 .47' },
-              [sameDate1]: { tranxId: '15889', loadAmount: '3318 .47' },
-              [date2]: { tranxId: '15883', loadAmount: '447' },
-              [date3]: { tranxId: '15866', loadAmount: '447' },
-              [sameDate3]: { tranxId: '1585', loadAmount: '447' }
-          }
-            let res = customer.countTranxDay(custObj)
-            assert.deepEqual(res, { '1999-12-31': 2, '2001-2-1': 1, '2001-1-31': 2 })
-        })
-        describe("verifyAmountDay", function(){
-            it("return false when more than 3 per day", function(){
-                customer.handleAccount("15888","$400 .47","2000-01-01T00:00:00Z",  "500" )
-                customer.handleAccount("15889","$3318 .47","1999-12-31T09:00:00Z",  "500" )
-                customer.handleAccount("15876","$400 .47","2000-01-01T01:00:00Z",  "500" )
-                let res = customer.handleAccount("15864","$400 .47","2000-01-01T02:00:00Z",  "500" )
-                assert(!res)
+describe("Koho tests", function() {
+	describe("addFunds()", function() {
+		it('returns an object', function() {
+			// let cusmtomer =
+			let res = customer.addFunds("1", "500", "200", "2000-01-01T00:00:00Z")
+			assert.deepEqual(res, { id: "200", tranxId: '1', loadAmount: '500', readAbleDate: "1999-12-31" })
+		})
+	})
+	describe("verifyUnder5000()", function() {
+		it("returns true if amount is under 5000", function() {
+			let res = customer.verifyUnder5000(4000)
+			assert(res)
+		})
+		it('returns false if amount is over 5000', function() {
+			let res = customer.verifyUnder5000(5001)
+			assert(!res)
+		})
+	})
+	describe("createAccount()", function() {
+		it("returns a customer acccount obj", function() {
+			let res = customer.createAccount("15888", "$3318 .47", "2001-03-01T00:00:00Z", "528", )
+			assert.deepEqual(res, { '2001-03-01T00:00:00Z': { id: "528", tranxId: '15888', loadAmount: '$3318 .47', readAbleDate: "2001-2-28" } })
+		})
+	})
+	describe("remove$()", function() {
+		it("returns string with dollar sign", function() {
+			let res = customer.remove$("$1000")
+			assert(res === "1000")
+		})
+	})
+	describe("makeDateObj()", function() {
+		it("returns date parts obj from date string", function() {
+			let res = customer.makeDateObj("1999-12-31T03:00:00Z")
+			console.log('res', res)
+			assert.deepEqual(res, "1999-12-30")
+		})
+	})
+	describe("getTranxDay()", function() {
+		it("returns object of transactions per day", function() {
+			let date1 = "2000-01-01T00:00:00Z"
+			let sameDate1 = "1999-12-31T09:00:00Z"
+			let date2 = "2001-02-01T06:00:00Z"
+			let date3 = "2001-02-01T04:00:00Z"
+			let sameDate3 = "2001-02-01T00:00:00Z"
+			let custObj = {
+				[date1]: { tranxId: '15888', loadAmount: '400 .47' },
+				[sameDate1]: { tranxId: '15889', loadAmount: '3318 .47' },
+				[date2]: { tranxId: '15883', loadAmount: '447' },
+				[date3]: { tranxId: '15866', loadAmount: '447' },
+				[sameDate3]: { tranxId: '1585', loadAmount: '447' }
+			}
+			let res = customer.getTranxDay(custObj)
+			assert.deepEqual(res, { '1999-12-31': 2, '2001-2-1': 1, '2001-1-31': 2 })
+		})
+		describe("verifyTranxDay()", function() {
+			it("return false when more than 3 per day", function() {
+				customer.handleAccount("15888", "$400 .47", "2000-01-01T00:00:00Z", "500")
+				customer.handleAccount("15889", "$3318 .47", "1999-12-31T09:00:00Z", "500")
+				customer.handleAccount("15876", "$400 .47", "2000-01-01T01:00:00Z", "500")
+				let res = customer.handleAccount("15864", "$400 .47", "2000-01-01T02:00:00Z", "500")
+				assert(!res)
 
-            })
-        })
-    })
+			})
+		})
+		describe("getDailySum()", function() {
+			it("returns sum of all daily transactions", function() {
+				let custObj = {
+					'2000-01-01T00:00:00Z': { id: '528', tranxId: '15888', loadAmount: '500.50' },
+					'1999-12-31T09:00:00Z': { id: '528', tranxId: '15889', loadAmount: '5' },
+					'2001-02-01T00:00:00Z': { id: '528', tranxId: '15883', loadAmount: '0.432' }
+				}
+				let res = customer.getDailySum(custObj, "1999-12-31T09:00:00Z")
+				assert(res === 505.932)
+			})
+		})
+		describe("verifyAmountPerDay()", function() {
+			it("returns true if total is under 5000", function() {
+				let custObj = {
+					'2000-01-01T00:00:00Z': {
+						id: '528',
+						tranxId: '15888',
+						loadAmount: '400.47',
+						readAbleDate: '1999-12-31'
+					},
+					'1999-12-31T09:00:00Z': {
+						id: '528',
+						tranxId: '15889',
+						loadAmount: '4500.47',
+						readAbleDate: '1999-12-31'
+					}
+				}
+				let res = customer.verifyAmountPerDay(custObj, "2000-01-01T00:00:00Z", customer.remove$("$700"))
+				console.log('RES', res)
+				assert(!res)
+			})
+			it("should not include new amount in DB", function() {
+				let custObj = {
+					'2000-01-01T00:00:00Z': {
+						id: '528',
+						tranxId: '15888',
+						loadAmount: '400.47',
+						readAbleDate: '1999-12-31'
+					},
+					'1999-12-31T09:00:00Z': {
+						id: '528',
+						tranxId: '15889',
+						loadAmount: '4500.47',
+						readAbleDate: '1999-12-31'
+					}
+				}
+
+				let res = customer.verifyAmountPerDay(custObj, "2000-01-01T00:00:00Z", customer.remove$("$700"))
+				assert(false)
+			})
+		})
+	})
 })
